81a87f6b9b2dc946d43eca8ab8a69c3fd4e974c8
Ashwani Kumar Dwivedi <ashwanidwivedi7898@gmail.com>
2024-08-02 02:28:05 +0530

    Project1 - Designed the homepage for online banking system

---
+++ Project 1 - Online Banking System/OnlineBankingSystem/src/main/java/com/banking/OnlineBankingSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.banking;
+
+/**
+ *
+ * @author Ashu
+ */
+public class OnlineBankingSystem {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new HomePage().setVisible(true);
+        });
+    }
+}
